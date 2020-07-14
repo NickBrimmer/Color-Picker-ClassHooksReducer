d8484bcf4ba2df57931b78f83da1d778d75b1670
Nicholas Brimmer <nicholasbrimmer@Nicholass-MacBook-Pro.local>
2020-07-14 15:22:49 -0700

    set up props for the components

--- Functions/src/containers/ColorPicker/ColorPicker.jsx
+++ Functions/src/containers/ColorPicker/ColorPicker.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from '../../components/Display/Display';
 import Controls from '../../components/Controls/Controls';
 
 export default function ColorPicker() {
+  const [color, setColor] = useState('#000000')
+  const [backgroundColor, setBackgroundColor] = useState('#FFFFFF')
+  const [text, setText] = useState('My really awesome text')
+
   return (
     <>
-      <Controls />
-      <Display />
+      <Controls
+        color={color}
+        backgroundColor={backgroundColor}
+        text={text}
+        onChange={}
+      />
+      <Display
+        color={}
+        backgroundColor={}
+        text={}
+      />
     </>
   )
 }
