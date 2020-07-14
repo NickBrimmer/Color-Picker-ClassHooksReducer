83ab19b9080ce61b75afe4492428c444579c0d72
Nicholas Brimmer <nicholasbrimmer@Nicholass-MacBook-Pro.local>
2020-07-14 15:19:20 -0700

    initial container scaffolding

---
+++ Functions/src/containers/ColorPicker/ColorPicker.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Display from '../../components/Display/Display';
+import Controls from '../../components/Controls/Controls';
+
+export default function ColorPicker() {
+  return (
+    <>
+      <Controls />
+      <Display />
+    </>
+  )
+}
