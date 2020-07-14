a31ecfaa64dec1e945cfe2e0491f9ab1f5de5ce5
Nicholas Brimmer <nicholasbrimmer@Nicholass-MacBook-Pro.local>
2020-07-14 15:23:13 -0700

    created hooks for state in the functional color picker

--- Functions/src/containers/ColorPicker/ColorPicker.jsx
+++ Functions/src/containers/ColorPicker/ColorPicker.jsx
@@ -3,9 +3,7 @@ import Display from '../../components/Display/Display';
 import Controls from '../../components/Controls/Controls';
 
 export default function ColorPicker() {
-  const [color, setColor] = useState('#000000')
-  const [backgroundColor, setBackgroundColor] = useState('#FFFFFF')
-  const [text, setText] = useState('My really awesome text')
+
 
   return (
     <>
