4420ccc439d273a4176195376859e30c6780fd6c
Nicholas Brimmer <nicholasbrimmer@Nicholass-MacBook-Pro.local>
2020-07-14 15:45:55 -0700

    ColorPicker custom hooks container built

--- Hooks/src/components/ColorPicker/ColorPicker.jsx
+++ Hooks/src/components/ColorPicker/ColorPicker.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Controls from '../Controls/Controls';
+import Display from '../Display/Display';
 
 
 export default function ColorPicker() {
   return (
     <>
+      <Controls
+        color={}
+        backgroundColor={}
+        text={}
+        onChange={}
+      />
+      <Display
+        color={}
+        backgroundColor={}
+        text={}
+      />
     </>
   );
 }
