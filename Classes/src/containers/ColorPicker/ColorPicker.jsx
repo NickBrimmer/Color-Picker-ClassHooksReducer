cf466ec16a339134c698f03c5da94873b969392e
Nicholas Brimmer <nicholasbrimmer@Nicholass-MacBook-Pro.local>
2020-07-14 14:59:52 -0700

    created initial component state

--- Classes/src/containers/ColorPicker/ColorPicker.jsx
+++ Classes/src/containers/ColorPicker/ColorPicker.jsx
@@ -3,7 +3,11 @@ import Controls from '../../components/Controls/Controls';
 import Display from '../../components/Display/Display';
 
 export default class ColorPicker extends Component {
-
+  state = {
+    color: '#000000',
+    backgroundColor: '#FFFFFF',
+    text: 'My Very Interesting App'
+  }
 
   render() {
     return (
