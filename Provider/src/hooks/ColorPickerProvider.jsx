98cb108a730fe84c6adfd7452103a8bd6a58c68b
Nicholas Brimmer <nicholasbrimmer@Nicholass-MacBook-Pro.local>
2020-07-14 19:35:45 -0700

    sending hook context items down as props

--- Provider/src/hooks/ColorPickerProvider.jsx
+++ Provider/src/hooks/ColorPickerProvider.jsx
@@ -1,13 +1,16 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState, useContext } from 'react';
 
 const ColorContext = createContext();
 
 export const ColorPickerProvider = ({ children }) => {
-
+  const [color, setColor] = useState('#000000');
+  const [backgroundColor, setBackgroundColor] = useState('#FFFFFF');
+  const [text, setText] = setText('My wonderful text!');
 
   return (
-    <ColorContext.Provider value={} >
+    <ColorContext.Provider value={{ color, backgroundColor, text }} >
       {children}
     </ColorContext.Provider>
   )
+
 }
